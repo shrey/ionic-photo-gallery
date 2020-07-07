2a7cc9e081820f5c83482359df8f26d17b2a6566
Shrey Dubey <wshrey09@gmail.com>
2020-07-07 14:59:10 +0530

    First

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import {PhotoService} from '../services/photo.service'
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  photos = this.photoService.photos;
 
-  constructor() {}
+  ngOnInit(){
+    this.photoService.loadSaved();
+  }
 
+  constructor(public photoService: PhotoService) { }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
